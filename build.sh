6a1cd010c05ca2cb31d546db7cb659d3b6f17a7d
scriptonist <aravindkp@outlook.in>
2019-02-22 13:57:53 +0530

    mend

--- build.sh
+++ build.sh
@@ -5,6 +5,4 @@ docker build -t hashnode-extensions-builder .
 
 docker run --name $IMAGE_NAME -it --rm -v "$(pwd)/build:/app/build" $IMAGE_NAME npm run build-all
 
-docker rm $IMAGE_NAME
-
-docker images rmi $IMAGE_NAME
+docker rmi $IMAGE_NAME

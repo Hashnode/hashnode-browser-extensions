079e6b36f3a88b7df0b36321bfe73a9fd03b87c0
scriptonist <aravindkp@outlook.in>
2019-02-22 14:46:25 +0530

    Friendly message

--- build.sh
+++ build.sh
@@ -5,4 +5,5 @@ docker build -t hashnode-extensions-builder .
 
 docker run --name $IMAGE_NAME -it --rm -v "$(pwd)/build:/app/build" $IMAGE_NAME npm run build-all
 
+echo "Cleaning up..."
 docker rmi $IMAGE_NAME

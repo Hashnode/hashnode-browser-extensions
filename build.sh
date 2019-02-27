ff7f3be9234755d654c7213675f3ecf655a05c5e
Girish Patil <devegpat@gmail.com>
2019-02-27 14:07:07 +0530

    Change mount folders

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ IMAGE_NAME="hashnode-extensions-builder"
 
 docker build -t hashnode-extensions-builder .
 
-docker run --name $IMAGE_NAME -it --rm -v "$(pwd)/build:/app/build" $IMAGE_NAME npm run build-all
+docker run --name $IMAGE_NAME -it --rm -v "$(pwd)/builds:/app/builds" $IMAGE_NAME npm run build-all
 
 echo "Cleaning up..."
 docker rmi $IMAGE_NAME

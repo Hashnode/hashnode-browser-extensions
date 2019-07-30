c230304bc4d01c1533e081d8cb866caf4de4e1d6
Girish Patil <devegpat@gmail.com>
2019-07-30 12:43:25 +0530

    Change tab positions

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      context: 'hot',
+      context: 'trending',
       posts: [],
       isLoading: false
     }
@@ -49,7 +49,7 @@ class App extends React.Component {
   }
 
   componentDidMount () {
-    this.fetchHotPosts()
+    this.fetchTrendingPosts()
   }
 
   render () {
@@ -68,8 +68,8 @@ class App extends React.Component {
             <img src={require('./images/hn-logo.png')} />
           </a>
           <div className='nav'>
-            <button className={this.state.context === 'hot' ? 'active' : ''} onClick={() => this.fetchHotPosts()}> Q&amp;A </button>
             <button className={this.state.context === 'trending' ? 'active' : ''} onClick={() => this.fetchTrendingPosts()}> Stories </button>
+            <button className={this.state.context === 'hot' ? 'active' : ''} onClick={() => this.fetchHotPosts()}> Q&amp;A </button>
           </div>
         </div>
         <div className='content'>

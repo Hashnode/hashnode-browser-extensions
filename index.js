9f84bf15d11f3e32d587da8dc4e3661f10d7095c
scriptonist <aravindkp@outlook.in>
2019-08-02 10:59:41 +0530

    Use revised graphql Query

--- index.js
+++ index.js
@@ -11,7 +11,8 @@ const cache = new InMemoryCache();
 const client = new ApolloClient({
   cache,
   link: new HttpLink({
-    uri: 'https://api.hashnode.com/graphql',
+    // uri: 'https://api.hashnode.com/graphql',
+    uri: 'http://localhost:4000/graphql',
     headers: {
       'client-name': 'Hashnode Browser Extension',
       'client-version': '0.0.1',
